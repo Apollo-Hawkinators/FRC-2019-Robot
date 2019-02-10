6ba454efef11451e57538b583357c3e1aa730f26
Hawkinators <apollohawkinators7547@gmail.com>
2019-02-09 17:05:54 -0700

    saved code

--- src/main/java/frc/robot/commands/ManualDrive.java
+++ src/main/java/frc/robot/commands/ManualDrive.java
@@ -11,15 +11,19 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //import frc.robot.ConstantAccelerationCalculator;
 import frc.robot.Operator;
+import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.subsystems.Drivetrain;
 
 public class ManualDrive extends Command {
 
 	private Operator operator = Operator.getOperator();
 	private Drivetrain drivetrain;
+
+	private Solenoid intakeSolenoid = new Solenoid(0);
+	private boolean isSolenoidOn = false;
 	//private ConstantAccelerationCalculator moveAcceleration = new ConstantAccelerationCalculator(0.0005);
 	//private ConstantAccelerationCalculator turnAcceleration = new ConstantAccelerationCalculator(0.0005);
-
+	
 	public ManualDrive() {
 		super("RampDrive");
 
